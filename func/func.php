e92a303183f04288850319499788f1db301c3a40
irakli-at-606344106867 <irakli@qcash.ge>
2020-02-03 18:44:30 +0400

    update to mysqli_connect

--- func/func.php
+++ func/func.php
@@ -1,7 +1,7 @@
 <?php
 	session_name("apiadmin");
 session_start();
-	$con = mysqli_connect("qcash-db.qcash.ge","qcashdbroot","hiepipuxae8ahkai2haiVoquuu5eigh9","admin_api");
+	$con = mysqli_connect("","","","");
 	mysqli_set_charset($con,"utf8");
 	date_default_timezone_set("Asia/Tbilisi");
 	$f=mysqli_real_escape_string($con,$_REQUEST["fname"]).".php";
