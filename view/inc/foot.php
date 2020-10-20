5983264ab97cc3bca890635bd5712f9a73e7256d
webdoors <ishal10@freeuni.edu.ge>
2020-10-20 14:29:32 +0400

    adding snackbar

--- view/inc/foot.php
+++ view/inc/foot.php
@@ -1,6 +1,6 @@
 <div id="snackbar"></div>
 	</body>
-	<script src="https://api.qcash.ge/admin/js/main.js?v=4"></script>
+	<script src="js/main.js?v=4"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="https://qcash.ge/js/bootbox.min.js"></script> 
+	<script src="js/bootbox.min.js"></script> 
 </html>
