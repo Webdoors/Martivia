3cf2b472fe3a09f0da63040f83c228c8e258b52b
Webdoors JSC <ishal10@freeuni.edu.ge>
2022-09-01 18:31:44 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ $(document).on("click",".BUT",function(){
 		message: '<p class="text-center">Please Wait...</p>',
 		closeButton: false
 	});	
-		func("Glogin",$(".USR").val(),$(".PAS").val(),$(".SMI").val());
+		func("Glogin",{a:$(".USR").val(),b:$(".PAS").val(),c:$(".SMI").val()});
 });
 $(document).on("click",".SMB",function(){
-	func("smslogin",$(".USR").val(),$(".PAS").val());
+	func("smslogin",{a:$(".USR").val(),b:$(".PAS").val()});
 });
 $(document).on("change",".FST",function(){
 	window.location.href="?page=reports&id=1&status="+$(this).val();
@@ -18,14 +18,14 @@ $(document).on("keyup","body",function(e){
 			message: '<p class="text-center">Please Wait...</p>',
 			closeButton: false
 		});	
-		func("Glogin",$(".USR").val(),$(".PAS").val(),$(".SMI").val());
+		func("Glogin",{a:$(".USR").val(),b:$(".PAS").val(),c:$(".SMI").val()});
 	}
 });
 $(document).on("click",".GLGT",function(){
 	func("Glogout");
 });
 $(document).on("keyup",".SAVCO",function(){
-	func("savecomment",$(this).attr("d"),$(this).val());
+	func("savecomment",{a:$(this).attr("d"),b:$(this).val()});
 });
 $(document).on("click",".CHE",function(){
 	
@@ -50,7 +50,7 @@ $(document).on("click",".CHE",function(){
 		message: 'დარწმუნებული ხართ?',
 		callback: function (result) {
 			if(result){
-				func("check",k,de);	
+				func("check",{a:k,b:de});	
 			}else{
 				window.location.reload();
 			}
@@ -108,7 +108,7 @@ $(document).on("click",".DEL",function(){
 		message: 'დარწმუნებული ხართ?',
 		callback: function (result) {
 			if(result){
-				func("delete",t,d);	
+				func("delete",{a:t,b:d});	
 			}
 		}
 	});		
@@ -157,140 +157,6 @@ function func(a,b,callback=null){
 			}
 		}
 	});
-}
-function func2(f, a, b, c, d, e, g, h, i, j, k, l, m) {
-		var FD = new FormData();
-		FD.append('fname',f);
-		if(a!=undefined){
-			FD.append('a',a);			
-		}
-		if(b!=undefined){		
-			FD.append('b',b);
-		}
-		if(c!=undefined){	
-			FD.append('c',c);
-		}
-		if(d!=undefined){			
-			FD.append('d',d);
-		}
-		if(e!=undefined){	
-			FD.append('e',e);
-		}
-		if(g!=undefined){	
-			FD.append('g',g);
-		}
-		if(h!=undefined){			
-			FD.append('h',h);
-		}
-		if(i!=undefined){	
-			FD.append('i',i);
-		}
-		if(j!=undefined){	
-			FD.append('j',j);
-		}
-		if(k!=undefined){	
-			FD.append('k',k);
-		}
-		if(l!=undefined){		
-			FD.append('l', l);
-		}
-		if(m!=undefined){			
-			FD.append('m', m);
-		}
-		$.ajax({  
-			type: "POST", 
-			cache: false,
-			contentType: false,
-			processData: false, 
-			url: "func/func.php",
-			data: FD, 
-			success: function (a) {
-			    if (a) {
-					if(f=="Glogout"){ 
-						if(a==1){
-							window.location.reload();
-						}
-					}
-					if(f=="delete"){ 
-						if(a==1){
-							window.location.reload();
-						}
-					}
-					if(f=="addmerchant"){ 
-						if(a==1){
-							window.location.reload();
-						}
-					}
-					if(f=="checks"){ 
-						if(a==1){
-							window.location.reload();
-						}
-					}
-					if(f=="addsector"||f=="DELSEC"){ 
-						if(a==1){
-							window.location.reload();
-						}
-					}
-					if(f=="delmerchant"||f=="deladmin"||f=="addadmin"){ 
-						if(a==1){
-							window.location.reload();
-						}
-					}
-					if(f=="notify"){
-						
-						bootbox.dialog({
-							message: a,
-							closeButton: true,
-							buttons: {		
-								confirm: {
-									label: 'OK',
-									className: 'btn-success'
-								}
-							}
-							
-						});								
-						
-					}
-					if(f=="Glogin"){
-						if(a==1){
-							window.location.reload();	
-						}else{
-							dialog.modal("hide");
-							bootbox.dialog({
-								message: 'Incorrect credentials!!!'
-							});									
-						}
-					}
-					if(f=="smslogin"){
-						if(a==1){
-							bootbox.alert('Sms code was sent!!!',function(){setTimeout(function(){$(".SMI").focus();},1000);} );									
-						}else{
-							bootbox.alert('Incorrect credentials!!!');								
-						}
-					}					
-					if(f=="getscore"){
-						bootbox.dialog({
-							message:a
-						});							
-					} 						
-					if(f=="getpermissions"){
-						bootbox.dialog({
-							message:a
-						});							
-					} 		
-					if(f=="getmatrix"){
-						bootbox.dialog({
-							message:a
-						});							
-					} 						
-				}else{
-					
-				}
-			},
-			error: function(XMLHttpRequest, textStatus, errorThrown) { 
-
-			} 
-		}); 
 }
 function menu(x) {
     x.classList.toggle("change");
