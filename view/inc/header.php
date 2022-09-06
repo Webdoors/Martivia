bea74f89ab5df40bab41472fa982ba2b9d03eaeb
Webdoors JSC <ishal10@freeuni.edu.ge>
2022-09-06 18:27:44 +0400

    Update header.php

--- view/inc/header.php
+++ view/inc/header.php
@@ -12,24 +12,20 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="content-language" content="ka">
 		<meta name="Author" content="Irakli Shalamberidze">
-		<meta property="og:title" content="Atelierkikala - Fashion House" />
-		<meta property="og:description" content="ატელიერ კიკალა მოდის სახლი" />
-		<meta name="keywords" content="ბიზნეს,საკრედიტო,ხაზი,რეგისტრაცია,მოთხოვნა,ლიმიტის,დამტკიცება,სესხი,ტრანში"/>
-		<meta name="description" content="ონლაინ ბიზნეს შეფასება 24/7" />
-		<meta name="author" content="QuickCash LLC" />
-		<meta name="copyright" content="&copy;2016 atelierkikala.com" />
+		<meta property="og:title" content="" />
+		<meta property="og:description" content="" />
+		<meta name="keywords" content=""/>
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+		<meta name="copyright" content="" />
 		<meta name="robots" content="all" />
 		<meta name="revisit-after" content="1 days" />
-		<link rel="canonical" href="https://atelierkikala.com/"/>
-		<meta property="og:image" content="https://atelierkikala.com/img/unnamed.jpg" />
+		<link rel="canonical" href=""/>
+		<meta property="og:image" content="" />
 		<meta property="og:type" content="website" />
-		<meta property="fb:app_id" content="248744675578367" />
-		<meta property="og:url" content="http://kikalastudio.com/" />
-		<link rel="stylesheet" type="text/css" href="http://kikalastudio.com/css/bootstrap.min.css"/>
+		<meta property="fb:app_id" content="" />
+		<meta property="og:url" content="" />
 		<link rel="stylesheet" type="text/css" href="css/style.css?v=12"/>
-		<link rel="stylesheet" type="text/css" href="http://kikalastudio.com/css/font-awesome/css/font-awesome.min.css"/>
-		<link rel="stylesheet" href="https://atelierkikala.com/supr/css/plugins.css">
 		<script src="https://code.jquery.com/jquery-latest.min.js"></script>
-		<script src="http://kikalastudio.com/js/jquery-migrate-1.2.1.js"></script>
 	</head>
 	<body>
